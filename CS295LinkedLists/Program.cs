75a72091c597d611488ba72ffaba59a580301897
MRivera <matias.g.rivera.civ@gmail.com>
2024-07-28 13:44:09 -0700

    Constructed all necessary elements and methods for LinkedList and Node to function properly. Deleting Program.cs conceptual writeup from 26:25

--- CS295LinkedLists/Program.cs
+++ CS295LinkedLists/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿// Linked List
+using CS295LinkedLists;
+
+// Construct a new node and assign a value to Data
+Node nodeA = new Node();
+nodeA.Data = 1234213;
+
+Node nodeB = new Node();
+nodeB.Data = 99999;
+
+Node nodeC = new Node();
+nodeC.Data = 4312;
+
+Node nodeD = new Node();
+nodeD.Data = 1111;
+
+// Link the nodes logically, from A thru D
+// A references B, B references C, C references D
+nodeA.Next = nodeB;
+nodeB.Next = nodeC;
+nodeC.Next = nodeD;
